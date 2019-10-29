2922e4392374c0a2ee60b8853c35483b604d0a87
lhx <1937506994@qq.com>
2019-10-29 14:17:55 +0800

    AOP面向方法测试

--- src/main/java/com/example/junittest/controller/LoginController.java
+++ src/main/java/com/example/junittest/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.example.junittest.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
